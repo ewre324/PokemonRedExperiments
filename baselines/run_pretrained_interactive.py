45dfeaa53879dbe274bb1ab2c37aec53a02d1f90
ewre324 <167686499+ewre324@users.noreply.github.com>
2025-03-13 15:09:40 +0530

    Update run_pretrained_interactive.py

--- baselines/run_pretrained_interactive.py
+++ baselines/run_pretrained_interactive.py
@@ -1,27 +1,18 @@
+import time
 from os.path import exists
 from pathlib import Path
 import uuid
 from red_gym_env import RedGymEnv
-from stable_baselines3 import A2C, PPO
-from stable_baselines3.common import env_checker
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3 import PPO
 from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3.common.callbacks import CheckpointCallback
 import cv2
 import numpy as np
 from google.colab.patches import cv2_imshow
+from IPython.display import clear_output
 
 def make_env(rank, env_conf, seed=0):
-    """
-    Utility function for multiprocessed env.
-    :param env_id: (str) the environment ID
-    :param num_env: (int) the number of environments you wish to have in subprocesses
-    :param seed: (int) the initial seed for RNG
-    :param rank: (int) index of the subprocess
-    """
     def _init():
         env = RedGymEnv(env_conf)
-        #env.seed(seed + rank)
         return env
     set_random_seed(seed)
     return _init
@@ -32,25 +23,24 @@ if __name__ == '__main__':
     ep_length = 2**23
 
     env_config = {
-                'headless': True, 'save_final_state': True, 'early_stop': False,
-                'action_freq': 24, 'init_state': '../has_pokedex_nballs.state', 'max_steps': ep_length, 
-                'print_rewards': True, 'save_video': False, 'fast_video': True, 'session_path': sess_path,
-                'gb_path': '../PokemonRed.gb', 'debug': False, 'sim_frame_dist': 2_000_000.0, 'extra_buttons': True
-            }
+        'headless': True, 'save_final_state': True, 'early_stop': False,
+        'action_freq': 24, 'init_state': '../has_pokedex_nballs.state', 
+        'max_steps': ep_length, 'print_rewards': True, 'save_video': False, 
+        'fast_video': True, 'session_path': sess_path, 'gb_path': '../PokemonRed.gb', 
+        'debug': False, 'sim_frame_dist': 2_000_000.0, 'extra_buttons': True
+    }
     
-    num_cpu = 1 #64 #46  # Also sets the number of episodes per training iteration
-    env = make_env(0, env_config)() #SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
-    
-    #env_checker.check_env(env)
+    num_cpu = 1
+    env = make_env(0, env_config)()
+
     file_name = 'session_4da05e87_main_good/poke_439746560_steps'
     
     print('\nloading checkpoint')
     model = PPO.load(file_name, env=env, custom_objects={'lr_schedule': 0, 'clip_range': 0})
         
-    #keyboard.on_press_key("M", toggle_agent)
     obs, info = env.reset()
     while True:
-        action = 7 # pass action
+        action = 7  # pass action
         try:
             with open("agent_enabled.txt", "r") as f:
                 agent_enabled = f.readlines()[0].startswith("yes")
@@ -59,10 +49,21 @@ if __name__ == '__main__':
         if agent_enabled:
             action, _states = model.predict(obs, deterministic=False)
         obs, rewards, terminated, truncated, info = env.step(action)
-        #env.render()
-        # Capture and display the current frame
-        frame = np.array(env.render(reduce_res=True, add_memory=True, update_mem=True))
-        cv2_imshow(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+        # Render and display at 5 FPS
+        frame = np.array(env.render(reduce_res=False, add_memory=True, update_mem=True))
+
+        if frame is not None:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert to BGR for OpenCV
+            frame = cv2.resize(frame, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+            clear_output(wait=True)  # Clear previous frame to avoid clutter
+            time.sleep(1)
+            cv2_imshow(frame)  # Display frame
+
+        #  Add a delay for 1 FPS (1 second / 1 frames)
+        time.sleep(1 / 1)
+
         if truncated:
             break
+            
     env.close()
